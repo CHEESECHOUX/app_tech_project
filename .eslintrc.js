14740b7bdf8f7d5cf009ea3211587472c329becf
cheesechoux <gsoosuu@gmail.com>
2024-03-08 00:27:21 +0900

    - 프로젝트 코드 품질 및 코드 스타일 일관성 유지를 위해 prettier, eslint 설정
    [prettier]
    1. prettier 설치
    $ npm install --save-dev prettier
    
    2. 프로젝트의 모든 파일에 prettier 적용
    $ npx prettier --write .
    
    [eslint]
    1. eslint 설치
    $ npm install eslint --save-dev
    
    2. 프로젝트에 eslint 적용
    $ npx eslint --fix .

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,30 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: 'tsconfig.json',
-    tsconfigRootDir: __dirname,
-    sourceType: 'module',
-  },
-  plugins: ['@typescript-eslint/eslint-plugin'],
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-  ],
-  root: true,
-  env: {
-    node: true,
-    jest: true,
-  },
-  ignorePatterns: ['.eslintrc.js'],
-  rules: {
-    '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-  },
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        project: 'tsconfig.json',
+        tsconfigRootDir: __dirname,
+        sourceType: 'module',
+    },
+    plugins: ['@typescript-eslint/eslint-plugin', 'simple-import-sort'],
+    extends: ['plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
+    root: true,
+    env: {
+        node: true,
+        jest: true,
+    },
+    ignorePatterns: ['.eslintrc.js'],
+    rules: {
+        'prettier/prettier': ['error', { printWidth: 150 }],
+        'eol-last': 2, // 파일 끝에 개행문자가 없을 경우 경고
+        'space-before-blocks': [2, 'always'], // 블록 앞에 공백을 강제
+        'function-paren-newline': ['error', 'consistent'], // 함수의 인자가 여러줄일 경우, 첫번째 인자는 첫줄에, 나머지는 각각 한줄씩
+        'object-property-newline': ['error', { allowAllPropertiesOnSameLine: false }], // 객체의 프로퍼티가 여러줄일 경우, 첫번째 프로퍼티는 첫줄에, 나머지는 각각 한줄씩
+        'no-empty': ['error', { allowEmptyCatch: false }], // 빈 catch 금지
+        '@typescript-eslint/interface-name-prefix': 'off',
+        '@typescript-eslint/explicit-function-return-type': 'off',
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
+        'simple-import-sort/imports': 'error',
+        'simple-import-sort/exports': 'error',
+    },
 };
