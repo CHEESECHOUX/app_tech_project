62bd5f4232d8178b29e7098f048da85f44eda0a9
cheesechoux <gsoosuu@gmail.com>
2024-03-08 00:58:18 +0900

    feat: validation pipe 애플리케이션으로 전송되는 모든 데이터의 정확성 검증을 위해 class-validator 패키지 활용
    $ npm i --save class-validator class-transformer

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
@@ -10,6 +11,15 @@ async function bootstrap() {
     app.useGlobalFilters(new AllExceptionsFilter());
 
     app.useGlobalInterceptors(new HttpLoggingInterceptor());
+
+    app.useGlobalPipes(
+        new ValidationPipe({
+            transform: true, // 원시 타입의 데이터를 DTO 클래스의 인스턴스로 변환
+            whitelist: true, // DTO에 정의되지 않은 속성이 요청 본문에 있을 경우, 자동으로 제거
+            transformOptions: { enableImplicitConversion: false }, // 타입 변환을 암시적으로 수행
+            disableErrorMessages: false,
+        }),
+    );
 
     await app.listen(3000);
 }
