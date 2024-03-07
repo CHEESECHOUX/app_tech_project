51182e75666d8d0510e5babafd91ebf40d8c9b6e
cheesechoux <gsoosuu@gmail.com>
2024-03-08 00:34:12 +0900

    feat: joi 패키지를 활용하여 환경변수 유효성 검사
    1. @nestjs/config의 NODE_ENV, PORT 유효성 검사
    
    2. joi 패키지 설치
    $ npm install joi

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -8,6 +9,10 @@ import { AppService } from './app.service';
     imports: [
         ConfigModule.forRoot({
             envFilePath: `${__dirname}/config/env/.env.${process.env.NODE_ENV}`,
+            validationSchema: Joi.object({
+                NODE_ENV: Joi.string().valid('development', 'production').default('development'),
+                PORT: Joi.number().default(3000),
+            }),
         }),
     ],
     controllers: [AppController],
