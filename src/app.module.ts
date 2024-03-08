85e2dc1ff6b91d34f0e65d23f3db63b864254aef
cheesechoux <gsoosuu@gmail.com>
2024-03-09 01:52:13 +0900

    feat: TypeORM 1. TypeORM 패키지 설치 $ npm install --save @nestjs/typeorm typeorm mysql2
    2. TypeORM에서 camel case 필드를 snake case 컬럼에 매핑해주는 typeorm-naming-strategies 패키지 설치
    $ npm install --save typeorm-naming-strategies

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from '@src/app.controller';
 import { AppService } from '@src/app.service';
 import * as Joi from 'joi';
+import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
 
 @Module({
     imports: [
@@ -11,6 +13,25 @@ import * as Joi from 'joi';
             validationSchema: Joi.object({
                 NODE_ENV: Joi.string().valid('development', 'production').default('development'),
                 PORT: Joi.number().default(3000),
+            }),
+        }),
+        TypeOrmModule.forRootAsync({
+            imports: [ConfigModule],
+            inject: [ConfigService],
+            useFactory: (configService: ConfigService) => ({
+                type: 'mysql',
+                host: configService.get('MYSQL_HOST'),
+                port: configService.get('MYSQL_PORT'),
+                username: configService.get('MYSQL_USERNAME'),
+                password: configService.get('MYSQL_PASSWORD'),
+                database: configService.get('MYSQL_NAME'),
+                entities: [__dirname + '/**/*.entity{.ts,.js}'],
+                synchronize: false, // 스키마 업데이트(데이터 삭제될 수 있음 주의)
+                autoLoadEntities: true, // 모듈 내에 정의된 엔티티 자동으로 로드
+                logging: true,
+                namingStrategy: new SnakeNamingStrategy(), // raw query를 사용하면 적용이 안 됨
+                useUTC: true, // UTC 시간 사용
+                timezone: 'Z', // UTC 시간대
             }),
         }),
     ],
